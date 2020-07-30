4bd8be12265581a15e997baa6e4ef88757b6d9d9
austtn <austin@aust.wtf>
2020-07-30 16:26:43 -0400

    refactor everything

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config()
 
-// Routes
 const api = require('./routes/api');
 const discord =require('./routes/discord');
 
+const app = express();
 
-const app = express(); //default app
 app.use(express.json());
 
-app.use('/api/', api)
+app.use('/api', api)
 app.use('/',discord)
 
 app.listen(process.env.PORT || 5000, () => console.log(`Listening on port: ${process.env.port}`))
